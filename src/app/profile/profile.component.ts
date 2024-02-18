61e93dc921360b51cce08bb5f000b0139b72b4f2
CedricRandriamanjaka <randriamanjakacedric@gmail.com>
2024-02-18 22:52:11 +0300

    favori

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,34 +9,41 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
     styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-// Déclarez un EventEmitter pour mettre à jour les compétences de l'utilisateur
-competencesUpdated: EventEmitter<any> = new EventEmitter<any>();
-
     closeResult: string;
     utilisateur: any;
     competences: any = [];
     competencesUtilisateur: any = [];
+
+    employes: any;
+    favoris:any;
+
+    competencesUpdated: EventEmitter<any> = new EventEmitter<any>();
+    favorisUpdated: EventEmitter<any> = new EventEmitter<any>();
     readonly ApiUrl = "http://localhost:3000/api/";
 
     constructor(private modalService: NgbModal, private http: HttpClient, private cookieService: CookieService) { }
 
     ngOnInit() {
-// Abonnez-vous à l'événement pour mettre à jour les compétences de l'utilisateur localement
-this.competencesUpdated.subscribe((competences: any) => {
-    this.competencesUtilisateur = competences;
-});
+        this.competencesUpdated.subscribe((competences: any) => {
+            this.competencesUtilisateur = competences;
+        });
+
+        this.favorisUpdated.subscribe((employes: any) => {
+            this.favoris = employes;
+        });
 
         const userId = this.cookieService.get('userId');
-        if (userId) { // Vérifiez si userId est défini pour éviter les requêtes inutiles
+        if (userId) {
             this.getUtilisateur(userId);
             this.getCompetencesUtilisateur(userId);
             this.getCompetences();
+            this.getEmployes();
+            this.getFavoris(userId);
         }
-
     }
 
     getUtilisateur(userId: string): void {
-        this.http.get<any>('http://localhost:3000/api/utilisateur/getUtilisateur/' + userId, {})
+        this.http.get<any>(this.ApiUrl + 'utilisateur/getUtilisateur/' + userId, {})
             .subscribe(
                 response => {
                     this.utilisateur = response;
@@ -73,35 +80,32 @@ this.competencesUpdated.subscribe((competences: any) => {
             );
     }
 
-    // ajouterCompetenceUtilisateur(competenceId: string): void {
-    //     const userId = this.utilisateur._id;
-    //     this.http.post(this.ApiUrl + 'profilEmployeretClient/competences/ajout/' + userId, { competenceID: competenceId })
-    //         .subscribe(
-    //             (data) => {
-    //                 console.log('Compétence ajoutée avec succès à l\'utilisateur:', data);
-    //                 // Actualiser la liste des compétences de l'utilisateur après l'ajout
-    //                 this.getCompetencesUtilisateur(userId);
-    //             },
-    //             (error) => {
-    //                 console.error('Erreur lors de l\'ajout de la compétence à l\'utilisateur:', error);
-    //             }
-    //         );
-    // }
+    getEmployes(): void {
+        this.http.get(this.ApiUrl + 'utilisateur/byRole/'+'2')
+            .subscribe(
+                (data) => {
+                    console.log('Data:', data);
+                    this.employes = data;
+                },  
+                (error) => {
+                    console.error('Error:', error);
+                }
+            );
+    }
 
-    // supprimerCompetenceUtilisateur(competenceId: string): void {
-    //     const userId = this.utilisateur._id;
-    //     this.http.post(this.ApiUrl + 'profilEmployeretClient/competences/supp/' + userId, { competenceID: competenceId })
-    //         .subscribe(
-    //             (data) => {
-    //                 console.log('Compétence supprimer avec succès à l\'utilisateur:', data);
-    //                 // Actualiser la liste des compétences de l'utilisateur après l'ajout
-    //                 this.getCompetencesUtilisateur(userId);
-    //             },
-    //             (error) => {
-    //                 console.error('Erreur lors de la suppresion de la compétence à l\'utilisateur:', error);
-    //             }
-    //         );
-    // }
+    getFavoris(userId: string): void {
+        this.http.get(this.ApiUrl + 'favori/utilisateur/' + userId)
+            .subscribe(
+                (data) => {
+                    console.log('url:', this.ApiUrl + 'favori/utilisateur/' + userId);
+                    this.favoris = data;
+                },
+                (error) => {
+                    console.error('Error:', error);
+                }
+            );
+    }
+
 
     ajouterCompetenceUtilisateur(competenceId: string): void {
         const userId = this.utilisateur._id;
@@ -125,8 +129,6 @@ this.competencesUpdated.subscribe((competences: any) => {
                 }
             );
     }
-    
-    
 
     supprimerCompetenceUtilisateur(competenceId: string): void {
         const userId = this.utilisateur._id;
@@ -140,6 +142,45 @@ this.competencesUpdated.subscribe((competences: any) => {
                 },
                 (error) => {
                     console.error('Erreur lors de la suppression de la compétence à l\'utilisateur:', error);
+                }
+            );
+    }
+
+    ajouterFavoriUtilisateur(empID: string): void {
+        const userId = this.utilisateur._id;
+        const favToAdd = this.employes.find(comp => comp._id === empID); // Trouver la compétence dans la liste complète des compétences
+    
+        this.http.post(this.ApiUrl + 'favori/ajout/' + userId, { empID: empID })
+            .subscribe(
+                (data) => {
+                    if(data){
+
+                        // Mise à jour locale des compétences de l'utilisateur
+                        if (favToAdd) {
+                            this.favoris.push(favToAdd); // Ajouter la compétence à la liste locale si elle existe
+                        }
+                        this.favorisUpdated.emit(this.favoris); // Émettre l'événement avec les données mises à jour
+                        console.log('favori utilisateur mises à jour:', JSON.stringify(this.favoris));
+                    }
+                },
+                (error) => {
+                    console.error('Erreur lors de l\'ajout favori à l\'utilisateur:', error);
+                }
+            );
+    }
+
+    supprimerFavoriUtilisateur(empID: string): void {
+        const userId = this.utilisateur._id;
+        this.http.post(this.ApiUrl + 'favori/supp/' + userId, { empID: empID })
+            .subscribe(
+                (data) => {
+                    console.log('favori supprimée avec succès à l\'utilisateur:', data);
+                    // Mise à jour locale des compétences de l'utilisateur
+                    this.favoris = this.favoris.filter(comp => comp._id !== empID); // Supprimez la compétence de la liste locale
+                    this.favorisUpdated.emit(this.favoris); // Émettez l'événement avec les données mises à jour
+                },
+                (error) => {
+                    console.error('Erreur lors de la suppression de favori à l\'utilisateur:', error);
                 }
             );
     }
