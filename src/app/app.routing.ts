68a163a9976df1aa45b31c0485d6cccd95edc9c1
MihobyF <mihobyfahasoavana@gmail.com>
2024-02-18 10:38:44 +0100

    route for manager-service-employee

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,6 +8,9 @@ import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { HomeManagerComponent } from './manager/home-manager/home-manager.component';
+import { ServicesComponent } from './manager/services/services.component';
+import { EmployesComponent } from './manager/employes/employes.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -15,6 +18,9 @@ const routes: Routes = [
   { path: 'register', component: SignupComponent },
   { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'manager', component: HomeManagerComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'employes', component: EmployesComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
