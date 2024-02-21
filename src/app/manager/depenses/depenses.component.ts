71a6d4f42f41b5f944b46fd4f18b4a9f32b5a01a
MihobyF <mihobyfahasoavana@gmail.com>
2024-02-21 18:05:57 +0100

    lister et filtrer depense

--- src/app/manager/depenses/depenses.component.ts
+++ src/app/manager/depenses/depenses.component.ts
@@ -23,17 +23,39 @@ export class DepensesComponent implements OnInit {
   depenseType: string = "";
   afficherAlerteChamp: boolean = false;
 
+  mois: number = 0;
+  jour: number = 0;
+
+  public days = Array.from({ length: 31 }, (_, i) => ({ value: (i + 1).toString().padStart(2, '0'), label: (i + 1).toString() }));
+
+  public months = [
+    { value: 1, label: 'Janvier' },
+    { value: 2, label: 'Février' },
+    { value: 3, label: 'Mars' },
+    { value: 4, label: 'Avril' },
+    { value: 5, label: 'Mai' },
+    { value: 6, label: 'Juin' },
+    { value: 7, label: 'Juillet' },
+    { value: 8, label: 'Août' },
+    { value: 9, label: 'Septembre' },
+    { value: 10, label: 'Octobre' },
+    { value: 11, label: 'Novembre' },
+    { value: 12, label: 'Décembre' },
+  ];
+
+
+
   depenses: any = [];
   typeDepenses: any = [];
 
   addDepenses() {
 
     var date = (<HTMLInputElement>document.getElementById("date")).value;
-      console.log("this.date"+this.date);
-      console.log("date"+date);
+      console.log("this.date"+this.date+"dskvd");
+      console.log("date"+date+"djvk");
 
 
-    if (this.description == "" || this.montant == "" || this.date == "") {
+    if (this.description == "" || this.montant == "" || date == "") {
       this.afficherAlerteChamp = true;
     }
 
@@ -43,21 +65,28 @@ export class DepensesComponent implements OnInit {
         depenseTypeID: this.depenseType,
         description: this.description,
         montant: this.montant,
-        date: this.date,
+        date: date,
       };
 
       var jsonData = JSON.stringify(dataObject);
       console.log('jsonData:' + jsonData);
 
-      // this.http.post(config.apiUrl + 'horaire/nouveauHoraire', jsonData, { headers: { 'Content-Type': 'application/json' } })
+      // this.http.post(config.apiUrl + 'depenses', jsonData, { headers: { 'Content-Type': 'application/json' } })
       //   .toPromise().then((res) => {
-      //     alert('Horaire ajouté');
-
+      //     alert('Dépense ajouté');
       // });
 
       this.getDepenses();
     }
 
+  }
+
+  filtreDepense(mois, jour) {
+    this.mois = mois;
+    this.jour = jour;
+    console.log( 'this.mois' + this.mois);
+    console.log( 'this.jour' + this.jour);
+    this.getDepenses();
   }
 
   getTypeDepenses() {
@@ -75,7 +104,7 @@ export class DepensesComponent implements OnInit {
   }
 
   getDepenses() {
-    this.http.get(config.apiUrl + 'depenses' )
+    this.http.get(config.apiUrl + 'depenses/mois=' + this.mois + '/jour=' + this.jour )
       .subscribe(
         (data: any) => {
           console.log('Depense:', data);
